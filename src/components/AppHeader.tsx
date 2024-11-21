1d584c3ed08914a0281d992bda9f46d2c301c215
Hitesh Khatri <hitesh.khatri@bmigroup.com>
2024-11-21 22:01:14 +0000

    app header component

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,12 @@
+export const AppHeader: React.FC = () => {
+  return (
+    <div className="rounded-lg bg-white p-6 text-surface">
+      <h2 className="mb-5 text-3xl font-semibold">Timezone Application</h2>
+      <p className="mb-4">
+        It uses utility classes for typography and spacing to space content out
+        within the larger container.
+      </p>
+      <hr className="my-6 h-0.5 border-t-0 bg-neutral-100 " />
+    </div>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,7 @@ import Sidebar from "../components/Sidebar";
 import TimezoneCard from "../components/TimezoneCard";
 import { useNavigate } from "react-router-dom";
 import { Timezone } from "../types/timezoneType";
+import { AppHeader } from "../components/AppHeader";
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate();
@@ -45,15 +46,8 @@ const HomePage: React.FC = () => {
     <div className="flex">
       <Sidebar />
       <div className="max-h-screen overflow-y-auto">
-        <div className="rounded-lg bg-white p-6 text-surface">
-          <h2 className="mb-5 text-3xl font-semibold">Timezone Application</h2>
-          <p className="mb-4">
-            It uses utility classes for typography and spacing to space content
-            out within the larger container.
-          </p>
-          <hr className="my-6 h-0.5 border-t-0 bg-neutral-100 " />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 ">
+        <AppHeader />
+        <div className="min-w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 ">
           {timezones.map((timezone) => (
             <TimezoneCard
               key={timezone.zoneName}
